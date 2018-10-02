1cb37ccb8ecb56b9febe73c408f88773b0b5004e
Josejgr27 <josegerardo0102@gmail.com>
2018-10-02 15:45:51 -0700

    E2-2 Bueno

---
+++ E2-2 Jose Gerardo Ramirez/E2-2 Jose Gerardo Ramirez/Fibonacci.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_2_Jose_Gerardo_Ramirez
+{
+    class Fibonacci
+    {
+        public void Iterativo(int Numero)
+        {
+            int Base1 = 0;
+            int Base2 = 1;
+            Console.WriteLine("1");
+            for (int Contador = 0; Contador < Numero-1; Contador++)
+            {
+                int Resultado = Base1;
+                Base1 = Base2;
+                Base2 = Resultado + Base2;
+                Console.WriteLine(Base2);
+            }
+        }
+
+        public void ConversionASegundos(DateTime Inicio, DateTime Final)
+        {
+            TimeSpan Duracion = Final - Inicio;
+            double SegundosTotales = Duracion.TotalSeconds;
+            Console.WriteLine("El programa tiene una duracion de {0} segundos", SegundosTotales);
+        }
+        public int Recursividad(int Resultado)
+        {
+            if (Resultado < 2)
+            {
+                return Resultado;
+            }
+            else
+            {
+                return Recursividad(Resultado - 1) + Recursividad(Resultado - 2);
+            }
+        }
+    }
+}
---
+++ E2-2 Jose Gerardo Ramirez/E2-2 Jose Gerardo Ramirez/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_2_Jose_Gerardo_Ramirez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci Fibonacci = new Fibonacci(); //Inicio del modo Iterativo
+            Console.Write("Introduzca las sumas requeridas del Fibonacci para ambos modos: ");
+            int NumeroDeSumas = int.Parse(Console.ReadLine());
+            DateTime InicioFor = DateTime.Now;
+            Fibonacci.Iterativo(NumeroDeSumas);
+            DateTime FinalFor = DateTime.Now;
+            Fibonacci.ConversionASegundos(InicioFor, FinalFor);
+            Console.ReadKey(); 
+            DateTime InicioRecursividad = DateTime.Now; //Inicio del modo Recursivo
+            int Numero = 1;
+            while(Numero<=NumeroDeSumas)
+            {
+             Console.WriteLine(Fibonacci.Recursividad(Numero));
+                Numero++;
+            }    
+            DateTime FinalRecursividad = DateTime.Now;
+            Fibonacci.ConversionASegundos(InicioRecursividad, FinalRecursividad);
+            Console.ReadKey();
+        }
+    }
+}
