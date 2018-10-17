7c5e0ab2ada887690b7208d38aab0ed8e50021cd
Josejgr27 <josegerardo0102@gmail.com>
2018-10-16 18:45:43 -0700

    E3-2 Jose Gerardo Ramirez

---
+++ E3-2 Jose Gerardo Ramirez/E3-2 Jose Gerardo Ramirez/Metodos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_2_Jose_Gerardo_Ramirez
+{
+    class Metodos
+    {
+        private Queue pcola; // Se declara la cola
+        public void Cola()
+        { 
+            pcola = new Queue(); // Se inicializa la cola
+        }
+
+        public void EnQueue(object item) // Añade un objeto a de cualquier tipo a la lista
+        {
+            pcola.Enqueue(item); // el objeto puede ser cualquier tipo de dato.
+        }
+
+        public void DeQueue()
+        {// Elimina el valor de la posicion 0 del arraylist
+            pcola.Dequeue();
+        }
+    }
+}
---
+++ E3-2 Jose Gerardo Ramirez/E3-2 Jose Gerardo Ramirez/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_2_Jose_Gerardo_Ramirez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue Cola = new Queue(); // Se crea la cola con el el nombre cola
+            for (int i = 0; i < 5; i++) // Se utiliza un for para llenar la Cola de cualquier tipo de dato
+            {
+                Console.Write("Introduzca un valor a la cola: ");
+                var Elemento = Console.ReadLine(); // Se lee el valor y se guarda en la variable de tipo Var llamada elemento
+                Cola.Enqueue(Elemento); // PAra asi mandar llamar la funcion Enqueue que guarda las variables del elemento que le den.
+            }
+
+            Cola.Dequeue(); // Elimina el primer elemento de la cola
+            foreach(var Elemento in Cola) // Foreach para imprimir los elementos de la cola
+            {
+                Console.WriteLine(Elemento);
+            }
+
+            Cola.TrimToSize(); // Establece la capacidad en el numero real de elementos que hay en la cola
+            Console.ReadKey();
+        }
+    }
+}
